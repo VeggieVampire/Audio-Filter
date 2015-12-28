bb53b1cd1a9b7e2a504b29b3ff073f5a7aaa1a03
VeggieVampire <VeggieVampire@users.noreply.github.com>
2015-12-27 19:36:13 -0600

    Create Audio_Filter.py

---
+++ Audio_Filter.py
@@ -0,0 +1,44 @@
+#This script cleans up audio files.
+#To displayed the audio plot you must have matplotlib installed.
+#For processing the audio you will need to install scipy
+#To Get updates go to https://github.com/VeggieVampire
+
+
+import scipy.io.wavfile
+import math
+
+rate, data = scipy.io.wavfile.read('input.wav')
+
+
+import matplotlib.pyplot as plt
+plt.plot(data)
+plt.show()
+
+
+
+UpClip = eval(raw_input("What upper value do you want to Clipping?: "))
+BottomClip = eval(raw_input("What bottom value do you want to Clipping?: "))
+
+#cut all values below user's value
+for n,i in enumerate(data):
+    if i<= BottomClip:
+        data[n]= BottomClip + 1
+
+#cut all values above user's value
+for n,i in enumerate(data):
+    if i >= UpClip:
+        data[n]= UpClip + 1
+
+
+
+
+#Show changed plot
+plt.plot(data)
+plt.show()
+
+#output a wave file
+from scipy.io.wavfile import write
+scipy.io.wavfile.write('output.wav',rate,data)
+
+
+
